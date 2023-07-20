c93101ba035aa184528e11561f2196082465f0eb
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-07-20 10:54:15 -0300

    feat: configuring autoplay

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -26,7 +26,7 @@ export function Module({ lessonsAmount, moduleIndex, title }: ModuleProps) {
   });
 
   return (
-    <Collapsible.Root className="group">
+    <Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
       <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-xs">
           {moduleIndex + 1}
